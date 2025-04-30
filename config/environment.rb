9c4a8b4ce34193f25e86ab45115157cf798d0f0a
Henrique Negri <henriquenegri84@gmail.com>
2025-04-30 13:44:34 -0300

    fix: rollback enviornment config

--- config/environment.rb
+++ config/environment.rb
@@ -1,12 +1,4 @@
 # Load the Rails application.
 require_relative "application"
-
-# Forçar o carregamento do arquivo .env
-require "dotenv"
-Dotenv.load
-
 # Initialize the Rails application.
 Rails.application.initialize!
-
-# Verificar se o SECRET_KEY_BASE está sendo carregado
-puts "SECRET_KEY_BASE: #{ENV['SECRET_KEY_BASE']}"
